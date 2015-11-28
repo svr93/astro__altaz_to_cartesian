56ef8323fb799e3f039a88038d1bf6b24667eb1a
vladimir shamsheev <shamsheev@MacBook-Pro-vladimir.local>
2015-11-28 19:32:36 +0300

    add_GetVelocityVector_func

--- translators/alt-az-translator.go
+++ translators/alt-az-translator.go
@@ -11,3 +11,18 @@ func ConvertAltAzRadiusToLocalCartezian(altR, azR, radius float64) (x, y, z floa
     z = radius * math.Cos(azR)
     return
 }
+
+func GetVelocityVector(altR, azR, radius, altRVel, azRVel, radiusVel float64) (x, y, z float64) {
+
+    x = radiusVel * math.Sin(azR) * math.Cos(altR) +
+        radius * azRVel * math.Cos(azR) * math.Cos(altR) -
+        radius * altRVel * math.Sin(azR) * math.Sin(altR)
+
+    y = radiusVel * math.Sin(azR) * math.Sin(altR) +
+        radius * azRVel * math.Cos(azR) * math.Sin(altR) +
+        radius * altRVel * math.Sin(azR) * math.Cos(altR)
+
+    z = radiusVel * math.Cos(azR) - radius * azRVel * math.Sin(azR)
+
+    return
+}
