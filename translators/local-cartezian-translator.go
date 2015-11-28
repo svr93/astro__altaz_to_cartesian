7a788c26e4b972c67934174294a36a12541d045b
vladimir shamsheev <shamsheev@MacBook-Pro-vladimir.local>
2015-11-29 00:21:37 +0300

    add_getDirectionCosMatrix_func,_replace_directionCosMatrix_variable

--- translators/local-cartezian-translator.go
+++ translators/local-cartezian-translator.go
@@ -11,8 +11,6 @@ const eqEarthRadiusKm = 6378.137
 
 const antennaAngleR = 0
 
-var directionCosMatrix = mat64.NewDense(3, 3, nil)
-
 type GeoPos struct {
 
     LatR float64
@@ -39,4 +37,43 @@ func convertGeoPosToCartesianPos(geoPos *GeoPos) (xKm, yKm, zKm float64) {
     zKm = (posEarthRadiusKm + geoPos.HKm) * math.Sin(geoPos.LngR)
 
     return
+}
+
+func getDirectionCosMatrix(geoPos *GeoPos) *mat64.Dense {
+
+    var val float64
+    directionCosMatrix := mat64.NewDense(3, 3, nil)
+
+    val = - math.Sin(geoPos.LngR) * math.Cos(geoPos.LatR) * math.Cos(antennaAngleR) -
+            math.Sin(geoPos.LatR) * math.Sin(antennaAngleR)
+    directionCosMatrix.Set(0, 0, val)
+
+    val =   math.Cos(geoPos.LngR) * math.Cos(geoPos.LatR)
+    directionCosMatrix.Set(0, 1, val)
+
+    val =   math.Sin(geoPos.LngR) * math.Cos(geoPos.LatR) * math.Sin(antennaAngleR) -
+            math.Sin(geoPos.LatR) * math.Cos(antennaAngleR)
+    directionCosMatrix.Set(0, 2, val)
+
+    val = - math.Sin(geoPos.LngR) * math.Sin(geoPos.LatR) * math.Cos(antennaAngleR) +
+            math.Cos(geoPos.LatR) * math.Sin(antennaAngleR)
+    directionCosMatrix.Set(1, 0, val)
+
+    val =   math.Cos(geoPos.LngR) * math.Sin(geoPos.LatR)
+    directionCosMatrix.Set(1, 1, val)
+
+    val =   math.Sin(geoPos.LngR) * math.Sin(geoPos.LatR) * math.Sin(antennaAngleR) +
+            math.Cos(geoPos.LatR) * math.Cos(antennaAngleR)
+    directionCosMatrix.Set(1, 2, val)
+
+    val =   math.Cos(geoPos.LngR) * math.Cos(antennaAngleR)
+    directionCosMatrix.Set(2, 0, val)
+
+    val =   math.Sin(geoPos.LngR)
+    directionCosMatrix.Set(2, 1, val)
+
+    val = - math.Cos(geoPos.LngR) * math.Sin(antennaAngleR)
+    directionCosMatrix.Set(2, 2, val)
+
+    return directionCosMatrix
 }
